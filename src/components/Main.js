ec8a2639dccf2e357d7d5a3cecc021e5dbab4a17
Deep Prajapati <dprajap2@gmail.com>
2022-01-27 15:32:15 -0500

    Feat: Set up search form and filters out airport codes from data

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,7 @@
+import './Main.css';
 import { useEffect, useState } from 'react';
-import './Main.css'
+import { sortData } from './data/sortData';
+import { yearsData } from './data/yearsData';
 const axios = require('axios');
 
 const Main = () => {
@@ -15,15 +17,49 @@ const Main = () => {
       console.log('Error in fetching the data ', error);
     }
   };
+  // Uses a sample data point to get all the airport codes
+  const allAirportCodes = allData
+    .filter((airportCodes) => airportCodes.Time.Label === '2003/06')
+    .map((airportCodes) => airportCodes.Airport.Code);
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <div className="main">
+  return (
+    <div className="main">
+      {allData.length ? (
+        <div>
+          <form>
+            <label htmlFor="condition"> Show </label>
+            <select name="conditions" id="conditions">
+              {sortData.map((condition) => {
+                return <option key={condition} value={condition} />;
+              })}
+            </select>
 
+            <label htmlFor="year">for</label>
+            <select name="year" id="year">
+              {yearsData.map((year) => {
+                return <option key={year} value={year} />;
+              })}
+            </select>
 
-  </div>;
+            <label htmlFor="airport-codes">at</label>
+            <select name="codes" id="codes" multiple size="2">
+              {allAirportCodes.map((code) => {
+                return <option value={code} key={code} />;
+              })}
+            </select>
+
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+      ) : (
+        <h1>Loading </h1>
+      )}
+    </div>
+  );
 };
 
 export default Main;
---
+++ src/components/data/sortData.js
@@ -0,0 +1,13 @@
+export const sortData = [
+  'Number of flights',
+  '% of flights on time',
+  ' % of flights on time',
+  '% of flights canceled',
+  '% of flights diverted',
+  '% of flights delayed',
+  '% of flights delayed due to carrier delay',
+  '% of flights delayed due to late aircraft',
+  '% of flights delayed due to weather',
+  '% of flights delayed due to security',
+  '% of flights delayed due to air traffic control',
+];
